e240450be7cdb5c8a71b0c4b1a905e470936e129
allenkim115 <154535686+allenkim115@users.noreply.github.com>
2025-02-19 12:47:05 +0800

    Update product.routes.ts

--- src/products/product.routes.ts
+++ src/products/product.routes.ts
@@ -10,12 +10,12 @@ productRouter.get('/products', async (req: Request, res: Response) => {
         const allProducts = await database.findAll();
         
         if (!allProducts) {
-            res.status(StatusCodes.NOT_FOUND).json({error: 'No products found' });
+            return res.status(StatusCodes.NOT_FOUND).json({error: 'No products found' });
         }
 
-         res.status(StatusCodes.OK).json({total :allProducts.length, allProducts});
+         return res.status(StatusCodes.OK).json({total :allProducts.length, allProducts});
     } catch (error) {
-          res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+          return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
     }
 })
 
@@ -24,12 +24,12 @@ productRouter.get("/product/:id", async (req: Request, res: Response) => {
         const product = await database.findOne(req.params.id);
 
         if (!product) {
-            res.status(StatusCodes.NOT_FOUND).json({error: 'Product does not exist' });
+            return res.status(StatusCodes.NOT_FOUND).json({error: 'Product does not exist' });
         }
 
-        res.status(StatusCodes.OK).json({product});
+        return res.status(StatusCodes.OK).json({product});
     }catch(error){
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
     }
 })
 
@@ -38,12 +38,12 @@ productRouter.post("/product", async (req: Request, res: Response) => {
         const { name, price, quantity, image} = req.body;
 
         if (!name || !price || !quantity || !image) {
-            res.status(StatusCodes.BAD_REQUEST).json({error: 'Please fill all fields' });
+            return res.status(StatusCodes.BAD_REQUEST).json({error: 'Please fill all fields' });
         }
         const newProduct = await database.create({ ...req.body });
-        res.status(StatusCodes.CREATED).json({newProduct});
+        return res.status(StatusCodes.CREATED).json({newProduct});
     }catch(error){
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error});
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error});
     }
 })
 
@@ -54,13 +54,13 @@ productRouter.put("/product/:id", async (req: Request, res: Response) => {
         const findProduct = await database.findOne(id);
 
         if (!findProduct) {
-            res.status(StatusCodes.NOT_FOUND).json({error: 'Product does not exist' });
+            return res.status(StatusCodes.NOT_FOUND).json({error: 'Product does not exist' });
         }
 
         const updatedProduct = await database.update(id, newProduct);
-        res.status(StatusCodes.OK).json({updatedProduct});
+        return res.status(StatusCodes.OK).json({updatedProduct});
     } catch(error){
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error});
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error});
     }
 })
 
@@ -69,13 +69,13 @@ productRouter.delete("/product/:id", async (req: Request, res: Response) => {
         const getProduct = await database.findOne(req.params.id);
 
         if (!getProduct) {
-            res.status(StatusCodes.NOT_FOUND).json({error: `No product with ID ${req.params.id}` });
+            return res.status(StatusCodes.NOT_FOUND).json({error: `No product with ID ${req.params.id}` });
         }
 
         await database.remove(req.params.id);
 
-        res.status(StatusCodes.OK).json({message: `Product with ID ${req.params.id} deleted`});
+        return res.status(StatusCodes.OK).json({message: `Product with ID ${req.params.id} deleted`});
     } catch(error){
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error});
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error});
     }
 })
